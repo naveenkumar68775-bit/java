fea8529388400f9b3c4702b3cd5937b562330728
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-07-15 22:50:24 +0530

    Origin

--- arr.java
+++ arr.java
@@ -41,3 +41,215 @@ import java.util.*;
 //         }
 //     }
 // }
+
+// public class arr{
+//     public static void main(String[] args) {
+//         int n=4;
+//         if(n>35){
+//             System.out.println("Pass");
+//         }
+//         else{
+//             System.out.println("Fail");
+//         }
+//     }
+// }
+
+// class arr{
+//     public static void main(String[] args) {
+//         int n=2026;
+//         if((n%4==0 )  || (n%100!=0 && n%400==0)){
+//             System.out.println("Leap year");
+//         }
+//         else{
+//             System.out.println("Not leap year");
+//         }
+//     }
+// }
+
+// class arr{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         if(n%5==0){
+//             System.out.println("Divisible: "+ n);
+//         }
+//         else{
+//             System.out.println("Not Divisible");
+//         }
+//     }
+// }
+
+// class arr{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner( System.in);
+//         char n=sc.next().charAt(0);
+//         if(n>='A' && 'Z'<=n){
+//             System.out.println("Alphabet");
+//         }
+//         else if (n>='a' && 'z'<=n){
+//             System.out.println("normal");
+//         }
+//         else{
+//             System.out.println("not ABCD");
+//         }
+
+//     }
+// }
+
+
+
+// public class arr{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = Integer.parseInt(sc.nextLine());
+//         int count = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             String line = sc.nextLine();
+
+//             if (line.endsWith("ERROR")) {
+//                 count++;
+//             }
+//         }
+
+//         System.out.println(count);
+//     }
+// }
+
+// class arr{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int sum=0;
+
+//         while(n>0){
+//             int v=n%10;
+//             if(n%2==0){
+//                 sum=sum+v;
+//             }
+//             n=n/10;
+
+//         }
+//         System.out.println(sum);
+//     }
+// }
+
+// class arr{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         for(int i=n;i>0;i--){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+// class arr{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int sum=0;
+//         for(int i=1;i<=n;i++){
+//             sum=sum+i;
+//         }
+//         System.out.println(sum);
+//     }
+// }
+
+// class arr{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int f=1;
+//         int n=sc.nextInt();
+//         for(int i=1;i<=n;i++){
+//             f=f*i;
+//         }
+//         System.out.println(f);
+//     }
+// }
+
+/**
+ * arr
+ */
+// public class arr {
+
+//      public static void main(String[] args) {
+//         int n=7;
+//         for(int i=1;i<=10;i++){
+//             System.out.println(i*7);
+//         }
+//      }
+// }
+
+/**
+ * arr
+ */
+// public class arr {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int v=0;
+//         while(n>0){
+//             v=n%10;
+//             System.out.print(v);
+//             n=n/10;
+//         }
+        
+
+
+//     }
+// }
+
+/**
+ * arr
+ */
+// public class arr {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int var=sc.nextInt();
+       
+//         int n=0;
+//         int v=1;
+//         for(int i=1;i<=var;i++){
+//             System.out.print(n+" ");
+//             int temp=n+v;
+
+//             n=v;
+//             v=temp;
+            
+//         }
+
+//     }
+// }
+
+/**
+ * arr
+ */
+// public class arr {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner( System.in);
+//         int n=sc.nextInt();
+//         for(int i=1;i<=n;i++){
+//             if(i%2==0){
+//                 System.out.print(i+" ");
+//             }
+//         }
+//     }
+// }
+
+// public class arr {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner( System.in);
+//         int n=sc.nextInt();
+//         for(int i=1;i<=n;i++){
+//             if(i%2!=0){
+//                 System.out.print(i+" ");
+//             }
+//         }
+//     }
+// }
--- array.java
+++ array.java
@@ -1,16 +1,159 @@
-package java.java;
+// package java.java;
+import java.lang.classfile.instruction.SwitchCase;
 import java.util.Scanner;
-public class array {
-    public static void main(String[] args) {
-        int sum=0;
-        Scanner levi=new Scanner(System.in);
-        int value=levi.nextInt();
-        int[]arr=new int[value];
-        for(int i=0;i<value;i++){
-            arr[i]=levi.nextInt();
-            sum=sum+arr[i];
-        }
+// public class array {
+//     public static void main(String[] args) {
+//         int sum=0;
+//         Scanner levi=new Scanner(System.in);
+//         int value=levi.nextInt();
+//         int[]arr=new int[value];
+//         for(int i=0;i<value;i++){
+//             arr[i]=levi.nextInt();
+//             sum=sum+arr[i];
+//         }
 
-        System.out.println(sum);
+//         System.out.println(sum);
+//     }
+// }
+
+
+/**
+ * array
+ */
+// public class array {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         switch (n) {
+//             case 1:
+//                 System.out.println("Monday");
+//                 break;
+//             case 2:
+//                 System.out.println("Tuesday");
+//                 break;
+//             case 3:
+//                 System.out.println("Wednesday");
+//                 break;
+//             case 4:
+//                 System.out.println("Thursday");
+//                 break;
+//             case 5:
+//                 System.out.println("Friday");
+//                 break;
+//             case 6:
+//                 System.out.println("Saturday");
+//                 break;
+//             case 7:
+//                 System.out.println("Sunday");
+//                 break;
+              
+        
+//             default:
+//                 break;
+//         }
+
+//     }
+// }
+
+/**
+ * array
+ */
+// public class array {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         int b=sc.nextInt();
+//         char v=sc.next().charAt(0);
+//         switch (v) {
+//             case '+':
+//                 System.out.println(a+b);
+//                 break;
+//             case '-':
+//                 System.out.println(a-b);
+//                 break;
+
+//         }
+
+//     }
+// }
+
+/**
+ * array
+ */
+// public class array {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+      
+//         char v=sc.next().charAt(0);
+//         switch (v) {
+//             case 'A':
+//                 System.out.println("Excellent");
+//                 break;
+//             case 'B':
+//                 System.out.println("Good");
+//                 break;
+//             case 'C':
+//                 System.out.println("Ok");
+//                 break;
+//             case 'D':
+//                 System.out.println("Never luck");
+//                 break;
+
+//             default:
+//                 break;
+//         }
+//     }
+// }
+
+// public class array {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+      
+//         char v=sc.next().charAt(0);
+//         switch (v) {
+//             case 'a':
+//                 System.out.println("vowel");
+//                 break;
+//             case 'e':
+//                 System.out.println("vowel");
+//                 break;
+//             case 'i':
+//                 System.out.println("vowel");
+//                 break;
+//             case 'o':
+//                 System.out.println("vowel");
+//                 break;
+//                 case 'u':
+//                 System.out.println("vowel");
+//                 break;
+
+//             default:
+//                 System.out.println("consonant");
+//                 break;
+//         }
+//     }
+// }
+
+/**
+ * array
+ */
+public class array {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(0);
+        switch (n%2){
+            case 0:
+                System.out.println("Even");
+                break;
+            default :
+            System.out.println("Odd");
+            break;
+        }
+    
     }
 }
