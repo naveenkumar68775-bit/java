b377ce1d85d0bde8017eea1deb3037b9221f41e3
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-07-17 12:00:49 +0530

    initial all

---
+++ th.java
@@ -0,0 +1,22 @@
+/**
+ * th
+ */
+import java.util.*;
+public class th {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []m=new int[n];
+        
+        for(int i=1;i<=n;i++){
+            m[n]=sc.nextInt();
+
+
+        
+        }
+        
+        Arrays.sort(m)
+        System.out.println();
+    }
+}
