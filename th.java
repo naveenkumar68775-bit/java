98a58e7532db5180398b215e2cb88e8218cf5525
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-07-18 14:18:21 +0530

    Value

--- th.java
+++ th.java
@@ -1,22 +1,177 @@
+// /**
+//  * th
+//  */
+// import java.util.*;
+// public class th {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int []m=new int[n];
+        
+//         for(int i=1;i<=n;i++){
+//             m[n]=sc.nextInt();
+
+
+        
+//         }
+        
+//         Arrays.sort(m)
+//         System.out.println();
+//     }
+// }
+
+import java.util.*;
+
 /**
  * th
  */
-import java.util.*;
+// public class th {
+//     String  name;
+//     th(){
+//         System.out.println("hello");
+//     }
+//     void add(String name){
+//         this.name=name;
+//     }
+
+//     public static void main(String[] args) {
+//         th s=new th();
+//         s.add("Itachi");
+//         System.out.println(s.name);
+        
+//     }
+// }
+
+/**
+ * th
+ */
+// class right{
+//     right(String v){
+//         System.out.println(v);
+//         System.out.println("Vetri");
+//     }
+// }
+// class it extends right{
+//     it(){
+//         super("Its");
+//         System.out.println("maran");
+//     }
+// }
+    
+// public class th {
+    
+    
+//     public static void main(String[] args) {
+//         it s=new it();
+        
+//     }
+// }
+
+/**
+ * th
+ */
+// class person{
+//     String name;
+//     person(String name) {
+//         this.name=name;
+//         System.out.println(name);
+
+//     }
+//     void odd(){
+//         System.out.println("dragon");
+//     }
+
+// }
+// class employee extends person{
+//     employee(String name){
+//         super(name);
+//         super.odd();
+//     } 
+// }
+// public class th {
+
+//     public static void main(String[] args) {
+//         employee v=new employee("Itachi");
+
+        
+//     }
+// }
+
+/**
+ * th
+ */
+// public class th {
+  
+//     static void th(){
+//         System.out.println("Hello");
+//     }
+//         static int v=10;
+    
+//     public static void main(String[] args) {
+//         th s=new th();
+//         s.th();
+//         System.out.println(v);
+//     }
+// }
+
+/**
+ * th
+ */
+// class v{
+//     static String s="Good day";
+
+// }
+// public class th {
+//     static {
+//         System.out.println("Hi");
+//     }
+//         static int a=10;
+//     public static void main(String[] args) {
+        
+    
+//         System.out.println(v.s);
+//         System.out.println(a);
+
+//     }
+// }
+
+/**
+ * th
+ */
+class person{
+    public String name ;
+    protected int age;
+    private int social_num;
+    String address;
+
+    person(String name,int age,int social_num,String address){
+        this.name=name;
+        this.age=age;
+        this.social_num=social_num;
+        this.address=address;
+        System.out.println(address);
+        // System.out.println(name);
+        // System.out.println(age);
+        // System.out.println(social_num);
+        // System.out.println(address);
+        
+    }
+}
+class employee extends person{
+    employee(){
+        super("Kakashi",21,777,"Leaf village,land of fire");
+        System.out.println("hello employee");
+    }
+}
 public class th {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int []m=new int[n];
-        
-        for(int i=1;i<=n;i++){
-            m[n]=sc.nextInt();
-
+        // person p=new person("Kakashi") ;
+        employee nav=new employee();
+        System.out.println(nav.name);
+        System.out.println(nav.age);
 
         
-        }
-        
-        Arrays.sort(m)
-        System.out.println();
     }
 }
---
+++ thunder.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+// public class thunder {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int m = sc.nextInt();
+//         int[][] v = new int[n][m];
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 v[i][j] = sc.nextInt();
+//             }
+//         }
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 System.out.print(v[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+
+//         sc.close();
+//     }
+// }
+
+/**
+ * thunder
+ */
+// public class thunder {
+
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int m=sc.nextInt();
+//         int v[][]=new int[n][m];
+
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 v[i][j]=sc.nextInt();
+//             }
+//         }
+//         int s=0;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 s=s+v[i][j];
+//             }
+            
+            
+//         }
+//         System.out.println(s);
+//     }
+// }
