8b2ab47a6eaef058f1877951274d89305f1488ee
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-07-24 14:05:45 +0530

    Java

---
+++ light.java
@@ -0,0 +1,9 @@
+class light{
+    public static void main(String[]args){
+       String a = new String("Java");
+String b = new String("Java");
+
+System.out.println(a == b);
+    }
+
+}
--- thunder.java
+++ thunder.java
@@ -50,3 +50,87 @@ import java.util.*;
 //         System.out.println(s);
 //     }
 // }
+
+// class thunder{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int m=sc.nextInt();
+//         int v[][]=new int[n][m];
+//         int max=v[0][0];
+       
+
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 v[i][j]=sc.nextInt();
+//             }
+//         }
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(v[i][j]>max){
+//                     max=v[i][j];
+//                 }
+//             }
+//         }
+//         System.out.println("Maximum value: "+max);
+        
+// }
+
+// import java.util.*;
+
+// public class thunder {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+//         int m = sc.nextInt();
+
+//         int[][] v = new int[n][m];
+
+//         // Input
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 v[i][j] = sc.nextInt();
+//             }
+//         }
+
+//         // Row Sum
+//         for (int i = 0; i < n; i++) {
+//             int sum = 0;
+
+//             for (int j = 0; j < m; j++) {
+//                 sum += v[i][j];
+//             }
+
+//             System.out.println("Row " + (i + 1) + " = " + sum);
+//         }
+
+//         sc.close();
+//     }
+// }
+class nav{
+    private int a=10;
+    private int b=20;
+
+    public int getA(){
+        return a;
+    }
+    public int getB(){
+        return b;
+    }
+    int c;
+    public nav (){
+        System.out.println(c=a+b);
+    }
+    
+
+}
+
+class thunder{
+    public static void main(String[] args) {
+        nav n=new nav();
+        System.out.println(n.getA());
+        System.out.println(n.getB());
+       
+    }
+}
