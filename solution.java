0c7ace900bd6f32245a9161eea1e456dbbbbd5a6
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-08-18 11:51:34 +0530

    light

--- solution.java
+++ solution.java
@@ -145,7 +145,7 @@ class solution{
         //     count++;
         // }
         // System.out.println(count);
-        Scanner sc=new Scanner(System.in);
+        // Scanner sc=new Scanner(System.in);
         // String n=sc.nextLine();
         // char v=sc.next().charAt(0);
         // int count=0;
@@ -170,11 +170,252 @@ class solution{
 
         //     }
         // }
+        // String name=sc.nextLine();
+        // String n[]=name.split("");
+        // String total="";
+        // for(int i=0;i<=name.length()-1;i++){
+        //     boolean v=false;
+        //     for(int j=i+1;j<=name.length()-1;j++){
+        //         if(n[i].equals(n[j])){
+        //             v=true;
+        //             break;
+                    
 
+        //         }
+              
 
+        //     }
+        //       if(!v){
+        //             total=total+n[i];
+        //         }
+        //     //  System.out.println(total);
+        // }
+        // System.out.println(total);
+
+        // String n="programming";
+        // String s[]=n.split("");
+
+        // for(int i=0;i<=n.length()-1;i++){
+        //     for(int j=i+1;j<=n.length()-1;j++){
+        //         if(s[i].equals(s[j])){
+        //             System.out.println(s[i]);
+        //             // break ;
+        //             // return;     // it can be stop
+        //         }
+        //         // break;
+        //     }
+        // }
      
 
+        // String s="silent";
+        // String s2="lisTEN";
 
-    }
+        // char []v1=s.toLowerCase().toCharArray();
+        // char []v2=s2.toLowerCase().toCharArray();
+
+        // Arrays.sort(v1);
+        // Arrays.sort(v2);
+
+        // if(Arrays.equals(v1,v2)){
+        //     System.out.println("Anagram");
+        // }
+        // else{
+        //     System.out.println("Not");
+        // Scanner sc=new Scanner(System.in);
+        // String s="banana";
+        // char []c=s.toCharArray();
+        // int count =0;
+        
+        // for(int i=0;i<=s.length()-1;i++){
+        //     for(int j=i+1;j<=s.length()-1;j++){
+        //     char ch=s.charAt(i);
+        //     char ch2=s.charAt(j);
+            
+        //     if(ch!=ch2){
+
+        //         count++;
+        //     }
+        //     else if(ch==ch2){
+        //         System.out.println(c[i] +"="+ count);
+
+        //     }
+        //     }
+
+        // }
+        // System.out.println(ch +"="+ count);
+
+        // int n=12345;
+
+        // int temp=n;
+        // int digit=0;
+        // for(int i=n;i>0;i--){
+        //     System.out.println(i);
+        // }
+        // System.out.println(digit);
+
+        // while(n>0){
+        //     n=n%10;
+        //     digit=digit*10+n;
+        //     n=n/10;
+            
+        // }
+        // System.out.println(digit);
+
+        //prime
+
+        // int n=22;
+        // int count=0;
+        // for(int i=1;i<=n;i++){
+        //     if(n%i==0){
+        //         count=count+1;
+                
+        //     }
+        //     }
+        
+        // if(count == 2){
+        //     System.out.println("prime");
+        // }
+        // else{
+        //     System.out.println("not");
+        // }
+
+        // int a=0;
+        // int b=1;
+        // int n=7;
+        // int digit;
+        // System.out.print(a + " " +b);
+        // for(int i=3;i<=n;i++){
+        //     digit=a+b;
+        //     System.out.print(" "+digit);
+        //     a=b;
+        //     b=digit;
+
+        // }
+
+        // int n=123945;
+        
+        // int count=0;
+        // while(n>0){
+        //     n=n/10;
+        //     count++;
+        // }
+        // System.out.println(count);
+        // int a=10;
+        // int b=20;
+
+        // int temp=a;
+        // a=b;
+        // b=temp;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // String b="Java programming language";
+        // String []v=b.split(" ");
+
+        // String l=v[0];
+        // String s=v[0];
+        // for(String i: v){
+        //     if(i.length()>l.length()){
+        //         l=i;
+        //     }
+        //     if(i.length()<s.length()){
+        //         s=i;
+        //     }
+        // }
+        // System.out.println(l);
+        // System.out.println(s);
+
+        // String t="hellol";
+        // for(int i=0;i<=t.length()-1;i++){
+        //     char ch=t.charAt(i);
+        //     if(t.indexOf(ch)==i){
+        //         int count=0;
+            
+        //     for(int j=0;j<=t.length()-1;j++){
+        //         if(t.charAt(j)==ch){
+        //             count++;
+        //         }
+        //     }
+        //     System.out.println(ch + "=" + count);
+        //     }
+           
+        // }
+
+        // String s="banaan";
+        // for(int i=0;i<=s.length()-1;i++){
+        //     char ch =s.charAt(i);
+        //     if(s.indexOf(ch)==i){
+        //         int count=0;
+        //         for(int j=0;j<=s.length()-1;j++){
+        //             if(s.charAt(j)==ch){
+        //                 count++;
+        //             }
+        //         }
+        //         System.out.println(ch +" = "+ count);
+        //     }
+        // }
+        
+        // String n="naveen";
+        // for(int i=0;i<=n.length()-1;i++){
+        //     char ch=n.charAt(i);
+        //     if(n.indexOf(ch)==i){
+        //         int count=0;
+        //         for(int j=0;j<=n.length()-1;j++){
+        //             if(n.charAt(j)==ch){
+        //                 count++;
+        //             }
+        //         }
+        //         System.out.println(ch + " " + count);
+        //     }
+        // }
+
+        // String n="thunder";
+        // int  c=n.length()-1;
+        // char s=n.charAt(c);
+        // System.out.println(s);
+
+    //     String a="hello";
+    //     // String[]v=a.split("");
+    //     // char []ch=a.toCharArray();
+   
+    //         String d=a.replaceAll("[aeiou]","*");
+    // System.out.println(d);
+    // String s="java programming language";
+    // String []v=s.split(" ");
+
+ 
+    // int ch=s.charAt(0);
+    // // String a=s.indexOf(0);
+    // String q=ch.
+
+    // String s="abcabcbb";
+    // char c[]=s.toCharArray();
+    // char t=c[0];
+    // for(int i=0;i<=s.length()-1;i++){
+    //     char ch=s.charAt(i);
+    //     if(t==ch){
+    //         System.out.println(ch);
+    //     }
+    // }
+
+    // String n="naveen";
+    // String v[]=n.split("");
+    // for(int i=0;i<=n.length()-1;i++){
+    //     for(int j=i+1;j<=n.length()-1;j++){
+    //         if(v[i].equals(v[j])){
+    //             System.out.println(v[i]);
+    //         }
+    //     }
+    // }
+    // String n="java programming language";
+    // String s[]=n.split(" ");
+
+    // for(String i:s){
+    //     System.out.print(Character.toUpperCase(i.charAt(0))+i.substring(1)+" ");
+
+    // }
+    
+}
+    
 
 }
