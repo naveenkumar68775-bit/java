6980970ce084f76ec940f6e0f827f357bd9877f2
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-08-26 21:59:11 +0530

    margin

--- school.java
+++ school.java
@@ -1,47 +1,58 @@
-// import java.util.Scanner;
-// public class school {
-//     String passorfail(int n){
-//         if(35>n){
-//             return "Fail";
-//         }
-//         else{
-//             return "PASS";
-//         }
-//     }
-//     public static void main(String[] args) {
-//         Scanner sc=new Scanner(System.in);
-//         int n=sc.nextInt();
-//         school v=new school();
-//         String result=v.passorfail(n);
-//         System.out.println(result);
+// // import java.util.Scanner;
+// // public class school {
+// //     String passorfail(int n){
+// //         if(35>n){
+// //             return "Fail";
+// //         }
+// //         else{
+// //             return "PASS";
+// //         }
+// //     }
+// //     public static void main(String[] args) {
+// //         Scanner sc=new Scanner(System.in);
+// //         int n=sc.nextInt();
+// //         school v=new school();
+// //         String result=v.passorfail(n);
+// //         System.out.println(result);
 
+// //     }
+// // }
+
+// class school{
+//     // String name;
+//     // school(String name){
+//     //     this.name=name;
+//     // }
+//     // static String name="Thunder";
+//     void mai(){
+//         System.out.println("Hello");
 //     }
-// }
+//     public static void main(String []args){
+//         // System.out.println();
+//     //     school v=new school("Ligthning");
+//     //     System.out.println(v.name);
+
+//     // }
+//     // try {
+//     //     int n=10/0;
+//     // }
+//     // catch(ArithmeticException e){
+//     //     System.out.println("Error");
+//     // }
+//     // finally{
+//     //     System.out.println("Hello");
+//     // }
+//     school v=new school();
+//         v.mai();
+// }}
+
 
 class school{
-    // String name;
-    // school(String name){
-    //     this.name=name;
-    // }
-    // static String name="Thunder";
     void mai(){
         System.out.println("Hello");
     }
     public static void main(String []args){
-        // System.out.println();
-    //     school v=new school("Ligthning");
-    //     System.out.println(v.name);
-
-    // }
-    // try {
-    //     int n=10/0;
-    // }
-    // catch(ArithmeticException e){
-    //     System.out.println("Error");
-    // }
-    // finally{
-    //     System.out.println("Hello");
-    // }
-    school v=new school();
+        school v=new school();
         v.mai();
-}}
+    }
+}
