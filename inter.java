a8cf1f097ede9ec1cc9656df5e21154533e9f1b9
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-08-28 21:55:55 +0530

    God

--- inter.java
+++ inter.java
@@ -10,38 +10,18 @@
 
 //     }
 // }
-import java .util.Scanner;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Stack;
-import java.util.Queue;
-import java.util.Set;
-/**
- * interface
- * 
- * 
- */
-import java.util.*;
-// public class inter{
-//     public static void main(String[] args) {
-        // Scanner sc=new Scanner(System.in);
-        
-    
-        // ArrayList<Integer> n=new ArrayList<>();
-        // n.add(10);
-        // System.out.println(n);
-        
-        // ArrayList v=(ArrayList)n.clone();
-        // System.out.println(v);
+// ArrayList<Integer> n=new ArrayList<>();
+// n.add(10);
+// System.out.println(n);
 
-        // HashMap <Integer ,Character> n=new HashMap<>();
-        // n.put(1,'n');
-        // n.put(2,'v');
-        // n.put(2,'s');
-        // System.out.println(n.get(n));
+// ArrayList v=(ArrayList)n.clone();
+// System.out.println(v);
 
+// HashMap <Integer ,Character> n=new HashMap<>();
+// n.put(1,'n');
+// n.put(2,'v');
+// n.put(2,'s');
+// System.out.println(n.get(n));
 
 //         Stack<Integer> v=new Stack<>();
 //         v.push(10);
@@ -50,124 +30,124 @@ import java.util.*;
 //         v.push (50);
 //         System.out.println(v);
 //         System.out.println(v.pop());
-       
+
 //         System.out.println( v.peek());
 //         System.out.println( v.pop());
 // System.out.println( v.pop());
 // System.out.println( v.pop());
 
-            // HashSet <Integer> v=new HashSet<>();
-            // v.add(10);
-            // v.add(20);
-            // v.add(30);
-            // v.add(10);
+// HashSet <Integer> v=new HashSet<>();
+// v.add(10);
+// v.add(20);
+// v.add(30);
+// v.add(10);
 
-            // v.remove(30);
-            // System.out.println(v);
+// v.remove(30);
+// System.out.println(v);
 
-            // Queue <Integer> v=new LinkedList<>();
-            // v.add(10);
-            // v.add(20);
-            // v.add(30);
-            // System.out.println(v);
-            // System.out.println(v.peek());
-            // System.out.println(v.poll());
-            //  System.out.println(v.poll());
+// Queue <Integer> v=new LinkedList<>();
+// v.add(10);
+// v.add(20);
+// v.add(30);
+// System.out.println(v);
+// System.out.println(v.peek());
+// System.out.println(v.poll());
+//  System.out.println(v.poll());
 
-            // Queue <Integer> n=new LinkedList<Integer>v.clone();
-            // Scanner sc=new Scanner(System.in);
-            // Set<Integer> v=new HashSet<>();
-            // int []n=new int[3];
-            // for(int i=0;i<=n.length-1;i++){
-            //     v.add(sc.nextInt());
-            // }
-            // System.out.println(v);
+// Queue <Integer> n=new LinkedList<Integer>v.clone();
+// Scanner sc=new Scanner(System.in);
+// Set<Integer> v=new HashSet<>();
+// int []n=new int[3];
+// for(int i=0;i<=n.length-1;i++){
+//     v.add(sc.nextInt());
+// }
+// System.out.println(v);
 
-            // int []n={10,5,8,25,20};
-            // int temp=0;
-            // for(int i=0;i<n.length-1;i++){
-            //     for(int j=0;j<n.length-i-1;j++){
-            //         if(n[j]>n[j+1]){
-            //             temp=n[j];
-            //             n[j]=n[j+1];
-            //             n[j+1]=temp;
-                        
-            //         }
+// int []n={10,5,8,25,20};
+// int temp=0;
+// for(int i=0;i<n.length-1;i++){
+//     for(int j=0;j<n.length-i-1;j++){
+//         if(n[j]>n[j+1]){
+//             temp=n[j];
+//             n[j]=n[j+1];
+//             n[j+1]=temp;
 
-            //     }
-            //     System.out.println(temp);
-            // }
+//         }
 
-            // int n=12345;
-            // while(n>0){
-            //     int v=n%10;
-            //     System.out.println(v);
-            //     n=n/10;
-            // }
+//     }
+//     System.out.println(temp);
+// }
 
-            // String s="naveen";
-            // String s1="";
-            // for(int i=s.length()-1;i>=0;i--){
-            //     s1=s1+s.charAt(i);
-            // }
-            // System.out.println(s1);
+// int n=12345;
+// while(n>0){
+//     int v=n%10;
+//     System.out.println(v);
+//     n=n/10;
+// }
 
-            // String s="thunder";
-            // String s1="";
-            // for(int i=s.length()-1;i>=0;i--){       
-                
-            //     s1=s1+s.charAt(i);
-            // }       
-            // System.out.println(s1);
+// String s="naveen";
+// String s1="";
+// for(int i=s.length()-1;i>=0;i--){
+//     s1=s1+s.charAt(i);
+// }
+// System.out.println(s1);
 
-            // String s="1212";
-            // String dup=s;
-            // String s1="";
-            // for(int i=s.length()-1;i>=0;i--){
-            //     s1=s1+s.charAt(i);
-            // }
-            // if(s1.equals(dup)){
-                
-            //     System.out.println("palindrome");
-            // }
-            // else{
-            //     System.out.println("not palindrome");
-            // }
-            
-            // Scanner n=sc.nextLine();
-            //  String s=sc.nextLine();
-            //  char []c=s.toCharArray();
-            // //  String total=""; 
-            //  char ch;   
-            //  int t=s.length()-1;                          
-            //  for(int i=0;i<=s.length()-1;i=i+2){
-            //     int value=s.indexOf(i);
-            //     for(int j=0;j<value;j++){
-            //         if(t%2!=0){
-            //             ch=s.charAt(i);
-            //             for(int k=0;k<=i;k++){
-            //                 System.out.println(ch);
-            //             }
-            //         }
-            //     }
-            // }
+// String s="thunder";
+// String s1="";
+// for(int i=s.length()-1;i>=0;i--){       
 
-            // String s="a3b2c6";
-            // char []ch=s.toCharArray();
-            // String total="";
-            // for(int i=0;i<=s.length()-1;i=i+2){
-            //     int index=s.indexOf(i);
-            //     for(int k=1;k<=s.length();k++){
-            //         if(k%2!=0){
-            //             char iam=s.charAt(i);
-            //             for(int j=1;j<=index;j++){
-            //                 System.out.println(iam);
+//     s1=s1+s.charAt(i);
+// }       
+// System.out.println(s1);
 
-            //         }
-            //     }
-            //     }
-              
-            // }
+// String s="1212";
+// String dup=s;
+// String s1="";
+// for(int i=s.length()-1;i>=0;i--){
+//     s1=s1+s.charAt(i);
+// }
+// if(s1.equals(dup)){
+
+//     System.out.println("palindrome");
+// }
+// else{
+//     System.out.println("not palindrome");
+// }
+
+// Scanner n=sc.nextLine();
+//  String s=sc.nextLine();
+//  char []c=s.toCharArray();
+// //  String total=""; 
+//  char ch;   
+//  int t=s.length()-1;                          
+//  for(int i=0;i<=s.length()-1;i=i+2){
+//     int value=s.indexOf(i);
+//     for(int j=0;j<value;j++){
+//         if(t%2!=0){
+//             ch=s.charAt(i);
+//             for(int k=0;k<=i;k++){
+//                 System.out.println(ch);
+//             }
+//         }
+//     }
+// }
+
+// String s="a3b2c6";
+// char []ch=s.toCharArray();
+// String total="";
+// for(int i=0;i<=s.length()-1;i=i+2){
+//     int index=s.indexOf(i);
+//     for(int k=1;k<=s.length();k++){
+//         if(k%2!=0){
+//             char iam=s.charAt(i);
+//             for(int j=1;j<=index;j++){
+//                 System.out.println(iam);
+
+//         }
+//     }
+//     }
+
+// }
 //             String s = "a3b2c6";
 // String total = "";
 
@@ -210,164 +190,162 @@ import java.util.*;
 // int n=sc.nextInt();
 // ArrayList<Integer> v=new ArrayList<>(n);
 // for(int i=0;i<n;i++){
-    
+
 //     v.add(sc.nextInt());
 // }
 // for(int i=0;i<n;i++){
-    // System.out.println(v);
+// System.out.println(v);
 
 // }
 
-            // int n=sc.nextInt();
-            // HashSet <Integer> v=new HashSet<>(n);
+// int n=sc.nextInt();
+// HashSet <Integer> v=new HashSet<>(n);
 
-            // for(int i=0;i<n;i++){
-            //     v.add(sc.nextInt());
-                
-            // }
-            // System.out.println(v);
-            // import java.util.*;
+// for(int i=0;i<n;i++){
+//     v.add(sc.nextInt());
 
+// }
+// System.out.println(v);
+// import java.util.*;
 
-        // Scanner sc = new Scanner(System.in);
+// Scanner sc = new Scanner(System.in);
 
-        // int n = sc.nextInt();
+// int n = sc.nextInt();
 
-        // int []v=new int[n];
+// int []v=new int[n];
 
-        // for(int i=0;i<n;i++){
-        //     v[i]=sc.nextInt();
+// for(int i=0;i<n;i++){
+//     v[i]=sc.nextInt();
 
-        // }
-        // for(int i=0;i<v.length;i++){
-        //     for(int j=i+1;j<v.length;j++){
-        //         if(v[i]==v[j]){
-        //             System.out.print("Count: "+v[i] +" ");
-        //         }
-        //     }
-        // }
-        // int n=sc.nextInt();
-        // ArrayList <Integer> v=new ArrayList<>(n);
-        // for(int i=0;i<n;i++){
-        //     v.add(sc.nextInt());
-        // }
-        // for(int i=0;i<n;i++){
-        //     for(int j=i+1;j<n;j++){
-        //         if(v.get(i)>v.get(j)){
-        //             int temp  =v.get(i);
-                    
+// }
+// for(int i=0;i<v.length;i++){
+//     for(int j=i+1;j<v.length;j++){
+//         if(v[i]==v[j]){
+//             System.out.print("Count: "+v[i] +" ");
+//         }
+//     }
+// }
+// int n=sc.nextInt();
+// ArrayList <Integer> v=new ArrayList<>(n);
+// for(int i=0;i<n;i++){
+//     v.add(sc.nextInt());
+// }
+// for(int i=0;i<n;i++){
+//     for(int j=i+1;j<n;j++){
+//         if(v.get(i)>v.get(j)){
+//             int temp  =v.get(i);
 
-        //         }
-        //     }
-        // }
+//         }
+//     }
+// }
 
-        // String n="1234";
-        // int a=n.length();
-        // System.out.print(a);
-        // int b=n.length()-1;
-        // System.out.print(b);
-        //  int c=n.length()-2;
-        // System.out.print(c);
-        //  int d=n.length()-3;
-        // System.out.print(d);
-        // String name ="naveen";
-        // char[]v=name.toCharArray();
-        // for(int i=0;i<=name.length();i++){
-        //     for(int j=i+1;j<name.length();j++){
-        //         if(v[i]==v[j]){
-        //             System.out.print(v[i]);
-        //         }
-        //     }
+// String n="1234";
+// int a=n.length();
+// System.out.print(a);
+// int b=n.length()-1;
+// System.out.print(b);
+//  int c=n.length()-2;
+// System.out.print(c);
+//  int d=n.length()-3;
+// System.out.print(d);
+// String name ="naveen";
+// char[]v=name.toCharArray();
+// for(int i=0;i<=name.length();i++){
+//     for(int j=i+1;j<name.length();j++){
+//         if(v[i]==v[j]){
+//             System.out.print(v[i]);
+//         }
+//     }
 
-        // }
+// }
 
-        // System.out.printf("My name is %s","God");
+// System.out.printf("My name is %s","God");
 
-        // System.out.println('a'+'b');
-        // System.out.println(3+((char)'a'));
-    // int size=6;
-    // int v[]=new int[8];
-    // v[0]=10;
-    // v[1]=80;
-    // v[2]=20;
-    // v[3]=30;
-    // v[4]=40;
-    // int num=100;
-    // int element=4;
-    // int size=6;
-    // if(element<size){
-    //     for(int j=size;j>element-1;j--){
-    //         v[j]=v[j-2];
-    //     }
-    //     v[element]=num;
-    //     size++;
-    // }
+// System.out.println('a'+'b');
+// System.out.println(3+((char)'a'));
+// int size=6;
+// int v[]=new int[8];
+// v[0]=10;
+// v[1]=80;
+// v[2]=20;
+// v[3]=30;
+// v[4]=40;
+// int num=100;
+// int element=4;
+// int size=6;
+// if(element<size){
+//     for(int j=size;j>element-1;j--){
+//         v[j]=v[j-2];
+//     }
+//     v[element]=num;
+//     size++;
+// }
 
-    // for(int i=0;i<v.length-1;i++){
-    //     System.out.println(v[i]);
-    // }
-    // System.out.println(v);/\\
-  
-    // int v[]=new int[10];
-    // v[0]=10;
-    // v[1]=80;
-    // v[2]=20;
-    // v[3]=30;
+// for(int i=0;i<v.length-1;i++){
+//     System.out.println(v[i]);
+// }
+// System.out.println(v);/\\
 
-    // int size=4;
-    // int index=2;
-    // int element =30;
-    // int element=50;
-    // if(index<size){
-    //     for(int i=size;i>index;i--){
-    //         v[i]=v[i-1];
-    //     }
-    //     v[index]=element;
-    //     size++;
-    // }
-    // for(int i=0;i<size;i++){
-    //     System.out.print(v[i]+" ");
-    // }
+// int v[]=new int[10];
+// v[0]=10;
+// v[1]=80;
+// v[2]=20;
+// v[3]=30;
 
-    // if(size>0){
-    //     for(int i=0;i<size-1;i++){
-    //         v[i]=v[i+1];
-    //     }
-        
-    // }
-    // for(int i=0;i<size-1;i++){
-    //     System.out.print(v[i]+ " ");
-    // }
+// int size=4;
+// int index=2;
+// int element =30;
+// int element=50;
+// if(index<size){
+//     for(int i=size;i>index;i--){
+//         v[i]=v[i-1];
+//     }
+//     v[index]=element;
+//     size++;
+// }
+// for(int i=0;i<size;i++){
+//     System.out.print(v[i]+" ");
+// }
 
-    // if(size>0){
-    //     for(int i=index;i<size;i++){
-    //         v[i]=v[i+1];
-    //     }
-    //     size--;
-    // }
-    // for(int i=0;i<size;i++){
-    //     System.out.print(v[i]+ " ");
-    // }
+// if(size>0){
+//     for(int i=0;i<size-1;i++){
+//         v[i]=v[i+1];
+//     }
 
-    // for(int i=0;i<size;i++){
-    //     if(v[i]==element){
-    //         System.out.println("index :"+i);
-    //     }
-    // }
-       
-    //   int []n={1,2,3,1,2,3,2,3,4};
-    //     for(int i=0;i<n.length;i++){
-    //         boolean nav=false;
-    //         for(int j=0;j<n.length;j++){
-    //             if(n[i]==n[j]){
-    //                 nav =true;
-    //                 break;
-    //             }
-    //         }
-    //         if(nav=false){
-    //             System.out.print(n[i]+" ");
-    //         }
-    //     }
+// }
+// for(int i=0;i<size-1;i++){
+//     System.out.print(v[i]+ " ");
+// }
+
+// if(size>0){
+//     for(int i=index;i<size;i++){
+//         v[i]=v[i+1];
+//     }
+//     size--;
+// }
+// for(int i=0;i<size;i++){
+//     System.out.print(v[i]+ " ");
+// }
+
+// for(int i=0;i<size;i++){
+//     if(v[i]==element){
+//         System.out.println("index :"+i);
+//     }
+// }
+
+//   int []n={1,2,3,1,2,3,2,3,4};
+//     for(int i=0;i<n.length;i++){
+//         boolean nav=false;
+//         for(int j=0;j<n.length;j++){
+//             if(n[i]==n[j]){
+//                 nav =true;
+//                 break;
+//             }
+//         }
+//         if(nav=false){
+//             System.out.print(n[i]+" ");
+//         }
+//     }
 //     }
 // }
 
@@ -408,11 +386,19 @@ import java.util.*;
 /**
  * inter
  */
-import java.util.LinkedList;
+/**
+ * interface
+ * 
+ * 
+ */
+import java.util.*;
+// public class inter{
+//     public static void main(String[] args) {
+        // Scanner sc=new Scanner(System.in);
 
-public class inter {
+// public class inter {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
 
         // LinkedList<Integer> numbers = new LinkedList<>();
 
@@ -473,6 +459,86 @@ public class inter {
 
 
         
+
+
+
+//     }
+// }
+
+import java.util.*;
+
+class inter{
+    public static void main(String[] args) {
+
+        // HashSet<Integer> set = new HashSet<>();
+
+        // set.add(10);
+        // set.add(20);
+        // set.add(10);
+        // set.add(30);
+
+        // System.out.println(set);
+//         TreeSet<Integer> set = new TreeSet<>();
+
+// set.add(50);
+// set.add(10);
+// set.add(30);
+// set.add(20);
+// set.add(30);
+
+// System.out.println(set);
+// Queue <Integer> n=new LinkedList<Integer>() ;
+Scanner sc=new Scanner(System.in);
+// Set<Integer> n=new TreeSet<>();
+// for(int i=0;i<7;i++){
+//     n.add(sc.nextInt());
+// }
+// System.out.println(n);
+
+// int []arr={10 ,20, 30, 20, 40, 10 ,50 ,30};
+// for(int i=0;i<arr.length;i++){
+//     for(int j=i+1;j<arr.length;j++){
+//         if(arr[i]==arr[j]){
+//             System.out.println(arr[i]);
+//         }
+//     }
+// }
+
+int arr[]={1 ,2 ,2 ,3 ,4 ,4 ,5 ,6 ,6};
+// for(int i=0;i<=arr.length-1;i++){
+//     for(int j=0;j<=arr.length-1;j++){
+//         if(arr[i]==arr[j]){
+            
+//             System.out.println();
+//         }
+//         else{
+//             System.out.println(arr[i]);
+//         }
+//     }
+// }
+
+// Set<Integer> n=new HashSet<>();
+// for(int i=0;i<=arr.length-1;i++){
+    
+// }
+List<Integer> a=new ArrayList<>();
+List<Integer> b=new ArrayList<>();
+
+System.out.print("A:");
+for(int i=0;i<5;i++){
+    a.add(sc.nextInt());
+}
+System.out.print("B:");
+for(int i=0;i<5;i++){
+    b.add(sc.nextInt());
+}
+System.out.println(a.retainAll(b));
+// for(int i=0;i<5;i++){
+//     if(a[i]==b[i]){
+
+//     }
+
+// }
 
 
 
