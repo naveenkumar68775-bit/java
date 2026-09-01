3461e5efff183116332ae08dde46a4b769d2430f
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-09-01 22:00:12 +0530

    thunder

--- oop.java
+++ oop.java
@@ -318,16 +318,31 @@
 
 /**
  * oop
+//  */
+// public class oop {
+
+//     public static void main(String[] args) {
+//         // int a=5;
+//         // int b=10;
+//         // a++;
+//         // b++;
+//         // System.out.println(a++ +b);
+//         //         System.out.println(a++ + ++b);
+
+//     }
+// }
+
+/**
+ * oop
  */
 public class oop {
 
     public static void main(String[] args) {
-        // int a=5;
-        // int b=10;
-        // a++;
-        // b++;
-        // System.out.println(a++ +b);
-        //         System.out.println(a++ + ++b);
-
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
+        float v=1000203f;
+        System.out.println(v);
     }
 }
