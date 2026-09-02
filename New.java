2d5821d785df3479c6f72a72f7b74ebe458567a0
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-09-02 23:04:53 +0530

    New

--- New.java
+++ New.java
@@ -1,5 +1,22 @@
+import java.util.*;
+
 class New {
-    public static void main(String[]args){
-        
-    }    
+
+    public static void main(String[] args) {
+
+        int[] input = {10, 20, 30, 20, 10};
+
+        HashMap<Integer, Integer> v = new HashMap<>();
+
+        for (int i = 0; i < input.length; i++) {
+
+            if (v.containsKey(input[i])) {
+                v.put(input[i], v.get(input[i]) + 1);
+            } else {
+                v.put(input[i], 1);
+            }
+        }
+
+        System.out.println(v);
+    }
 }
