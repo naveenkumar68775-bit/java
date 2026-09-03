88a08d34b58a1e75aea08d018d28221cb6f8a9bd
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-09-03 12:13:14 +0530

    New

--- New.java
+++ New.java
@@ -1,22 +1,70 @@
-import java.util.*;
+// import java.util.*;
 
-class New {
+// class New {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
 
-        int[] input = {10, 20, 30, 20, 10};
+//         int[] input = {10, 20, 30, 20, 10};
 
-        HashMap<Integer, Integer> v = new HashMap<>();
+//         HashMap<Integer, Integer> v = new HashMap<>();
 
-        for (int i = 0; i < input.length; i++) {
+//         for (int i = 0; i < input.length; i++) {
 
-            if (v.containsKey(input[i])) {
-                v.put(input[i], v.get(input[i]) + 1);
-            } else {
-                v.put(input[i], 1);
-            }
+//             if (v.containsKey(input[i])) {
+//                 v.put(input[i], v.get(input[i]) + 1);
+//             } else {
+//                 v.put(input[i], 1);
+//             }
+//         }
+
+//         System.out.println(v);
+//     }
+// }
+//import java.util.*;
+//
+//class New{
+//    public static void main(String [] args){
+//    	
+//    }
+//}
+// import java.util.HashMap;
+
+// package New.java;
+// import java.util.HashMap;
+
+// public class New {
+//     public static void main(String[] args) {
+
+//         int[] v = {10, 20, 10, 30, 20, 10};
+
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for (int i = 0; i < v.length; i++) {
+
+//             if (map.containsKey(v[i])) {
+
+//                 map.put(v[i], map.get(v[i]) + 1);
+
+//             } else {
+
+//                 map.put(v[i], 1);
+//             }
+//         }
+
+//         System.out.println(map);
+//     }
+// }
+
+import java.util.HashMap;
+
+class New{
+    public static void main(String[]arge){
+        int []n={10 ,20 ,10, 30, 20, 10 ,40 ,20 ,20};
+        HashMap <Integer,Integer> v=new HashMap<>();
+        int last=n[0];
+        for(int i=0;i<n.length;i++){
+            
+
         }
-
-        System.out.println(v);
     }
 }
