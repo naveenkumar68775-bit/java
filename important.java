da7f37d1f78d840e07c7231d4e70a2399b530e79
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-09-09 22:16:29 +0530

    light

---
+++ important.java
@@ -0,0 +1,107 @@
+class important {
+    public static void main(){
+        // String name="naveen".toUpperCase();
+        // System.out.println(name.charAt(0) +""+ name.charAt(2));
+        // string s=name.toUpperCase();
+
+        // char c='c';
+        // System.out.println(c);
+
+        // String a="nav";
+        // String b=a;
+        // System.out.println(a==b);
+
+        // String a="naveen";
+        // String b="neevan";
+        // // System.out.println(a.lastIndexOf("n"));
+        // System.out.println(a.compareTo(b));
+
+
+        // String s="programming";
+        // int count=0;
+        // for(int i=0;i<s.length();i++){
+        //     count++;
+        // }
+        // System.out.println(count);
+
+        // StringBuilder b=new StringBuilder("java");
+        // System.out.println(b.reverse());
+
+        // String p="programming";
+        // int c=p.length()-1;
+        // System.out.println(p.charAt(c));
+
+        // String n="Java is easy";
+        // String[] arr=n.split(" ");
+        // String s="";
+        // for(String v:arr){
+        //     s=s+v;
+        // }
+        // System.out.println(s);
+
+        // String s="programming";
+        // String add="";
+        // for(int i=0;i<=s.length()-1;i++ ){
+        //     char ch=s.charAt(i);
+        //     if(add.indexOf(ch)==-1){
+        //         add+=ch;
+        //     }
+        // }
+        // System.out.println(add);
+
+        // String name="programming";
+        // for(int i=0;i<=name.length()-1;i++){
+        //     for(int j=i+1;j<name.length();j++){
+        //         if(name.charAt(i)!=name.charAt(j)){
+        //             System.out.println(name.charAt(i));
+        //             return ;
+        //         }
+                
+        //     }
+           
+        // }
+
+    //     String name="banana";
+    //     char [] c=name.toCharArray();
+    //    for(int i=0;i<name.length();i++){
+    //     char ch=c[i];
+       
+    //     if(i==name.indexOf(ch)){
+    //          int count=1;
+    //         for(int j=i+1;j<c.length;j++){
+    //             if(ch==c[j]){
+    //                 count++;
+    //             }
+    //         }
+    //          System.out.println(ch+" "+count);
+
+    //     }
+    //    }
+
+    // String name = "apple";
+    // for(int i=0;i<name.length();i++){
+    //     char ch=name.charAt(i);
+    //     // int count+=i;
+    //     System.out.println(name.indexOf(ch));
+    // }
+    // }
+    String a="renduth";
+    String b="thunder";
+    int count=0;
+    for(int i=0;i<a.length();i++){
+        char ch=a.charAt(i);
+        for(int j=0;j<b.length();j++){
+            if(ch==b.charAt(j)){
+                count++;
+                
+            }
+        }
+    }
+    if(count==a.length()){
+        System.out.println("Anagram");
+    }
+    else{
+        System.out.println("Not");
+    }
+    }
+}
